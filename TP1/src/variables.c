bd270c4081730b6a6578f6fd72fb82a28f32a0ed
ropant <a.ropers05@gmail.com>
2026-01-29 10:45:46 +0100

    Update variables.c

---
+++ TP1/src/variables.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+main(){
+char chartest ="A";
+
+short shorttest = 12;
+
+int inttest = 1;
+
+long int longinttest =123;
+
+long long int longlonginttest = 12345667;
+
+float floattest =23.3;
+
+double doubletest=45.45;
+
+long double longdoubletest =456.456;
+
+printf("%c", chartest);
+printf("%d", shorttest);
+printf("%d", inttest);
+printf("%ld", longinttest);
+printf("%lld", longlonginttest);
+
+  return 0;
+}
