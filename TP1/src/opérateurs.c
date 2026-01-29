78cf9bbe287b8df88635d1560b0fbea6f204dce5
ropant <a.ropers05@gmail.com>
2026-01-29 10:57:53 +0100

    Update opérateurs.c

--- TP1/src/opérateurs.c
+++ TP1/src/opérateurs.c
@@ -1,2 +1,14 @@
+main(){
+  int a = 23;
+  int b = 21;
+  printf("Addition : %d\n", a + b);
+printf("Soustraction : %d\n", a - b);
+printf("Multiplication : %d\n", a * b);
+printf("Division : %d\n", a / b);
+printf("Modulo : %d\n", a % b);
+printf("a est égal à b : %d\n", a == b);
+printf("a est supérieur à b : %d\n", a > b);
+
+}
 
 
